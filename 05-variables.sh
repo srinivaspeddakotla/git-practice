949cfc09c7d2394f17ed4a3f2e6de3134d98e73f
Srinivas <psr.0342@gmail.com>
2024-11-24 12:41:32 +0530

    new variables usrpswd added

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read USERNAME #takes input into USERNAME variable
+
+echo "Username entered is: $USERNAME"
+
+echo "please enter your password::"
+
+read PASSWORD #takes input into PASSWORD variable
+
+echo "Username entered is: $PASSWORD"
+
