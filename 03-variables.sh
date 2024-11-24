772b9afb0c3a62f9cd1bf352d381fa27a1126731
Srinivas <psr.0342@gmail.com>
2024-11-24 12:04:33 +0530

    added phrases

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 person1=Raheem
 person2=suresh
 
-echo "$person1:: Hi $person2, How are you"
-echo "$person2:: Hello $person1, i am fine, How are you doing" 
-echo "$person1:: i am doing good $person2, what's going on"
-echo "$person2:: i started learning shell script $person1"
+echo "$person1:: Hi ${person2}, How are you"
+echo "${person2}:: Hello $person1, i am fine, How are you doing" 
+echo "$person1:: i am doing good ${person2}, what's going on"
+echo "${person2}:: i started learning shell script $person1"
