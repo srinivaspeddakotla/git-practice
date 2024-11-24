6ea7b1cab141b749ee59a8984e613476c91e0f33
Srinivas <psr.0342@gmail.com>
2024-11-24 12:19:43 +0530

    new variables script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi ${person2}, How are you"
+echo "${person2}:: Hello $person1, i am fine, How are you doing" 
+echo "$person1:: i am doing good ${person2}, what's going on"
+echo "${person2}:: i started learning shell script $person1"
