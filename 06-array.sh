a7f0adcf9b9b939d5bfff31ce51204c2fdd8fdb8
Srinivas <psr.0342@gmail.com>
2024-11-24 13:00:24 +0530

    array script

---
+++ 06-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#index (postion) starts from 0, size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+
+echo "First fruit is: ${FRUIT[0]}"
+
+echo "Second fruit is: ${FRUIT[1]}"
+
+echo "Third fruit is: ${FRUIT[2]}"
+
+echo "First fruit is: ${FRUIT[@]}"  # @ means all list will print
+
