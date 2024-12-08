4f634a203017e39f833c8078d534c722ad088f99
Srinivas <psr.0342@gmail.com>
2024-12-08 10:21:09 +0530

    Modules Funtions script added

--- 12-functions.sh
+++ 12-functions.sh
@@ -21,39 +21,26 @@ fi
 
 dnf list installed git
 
-VALIDATE $? "Listing Git"
 
-# if [ $? -ne 0 ]
-# then
-#     echo "Git is not installed, going to install it.."
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "Git installation is not success....check it"
-#         exit 1
-#     else 
-#         echo "Git installation is success"
-#     fi
-# else 
-#     echo "Git is already installed, nothing to do.."
-# fi
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "installing Git"
+else 
+    echo "Git is already installed, nothing to do.."
+fi
 
-# dnf installed mysql
+dnf installed mysql
 
-# if [ $? -ne 0 ]
-# then
-#     echo "MYSQL is not installed, going to install it.."
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "MYSQL installation is not success....check it"
-#         exit 1
-#     else 
-#         echo "MYSQL installation is success"
-#     fi
-# else 
-#     echo "MYSQL is already installed, nothing to do.."
-# fi
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed, going to install it.."
+    dnf install mysql -y
+    VALIDATE $? "installing MySQL"
+else 
+    echo "MYSQL is already installed, nothing to do.."
+fi
 
 
 
