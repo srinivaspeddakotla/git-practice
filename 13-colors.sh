6846aaa99271b5d0c8ff19734f2989d9d0df10d1
Srinivas <psr.0342@gmail.com>
2024-12-08 11:09:21 +0530

    Modules colors script added

---
+++ 13-colors.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "user ID is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Please run this script with root privileges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is...$R FAILED $N"
+        exit 1
+    else 
+        echo "$2 is...$G SUCCESS $N"
+    fi 
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "installing Git"
+else 
+    echo "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed, going to install it.."
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else 
+    echo "MYSQL is already installed, nothing to do.."
+fi
+
+
+
