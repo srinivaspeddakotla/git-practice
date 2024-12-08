3b18a19e3462e2c743fa4c7c3870287b5a3a3f19
Srinivas <psr.0342@gmail.com>
2024-12-08 15:44:00 +0530

    module loops script added

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
-echo "8"
-echo "9"
-echo "10"
+for i in {0..10} #0-10 
+do 
+    echo $i
+done
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
